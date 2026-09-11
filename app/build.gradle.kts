e8b999b49c6135d510e32ae819e9e56cacfec020
mjmalbaiti <mjmalbaiti@gmail.com>
2026-09-11 18:35:28 +0000

    Set Java compatibility to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 }
 
 android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     namespace = "com.duofoldmotion.app"
     compileSdk = 35
 
